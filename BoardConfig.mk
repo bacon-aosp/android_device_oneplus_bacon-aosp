bcd59e7294c2567153b2c08b5cdc9ad65df75cca
freshgiammi <rengucci.gianmarco@gmail.com>
2017-01-19 15:12:40 +0100

    Bacon: enable SEpolicies
    Initial bringup
    
    Signed-off-by: freshgiammi <rengucci.gianmarco@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -172,10 +172,10 @@ TARGET_DUMMY_RECOVERY_PATCH := true
 TARGET_NO_RPC := true
 
 # SELinux
-#include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/sepolicy.mk
 
-#BOARD_SEPOLICY_DIRS += \
-#    $(PLATFORM_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += \
+    $(PLATFORM_PATH)/sepolicy
 
 # Wifi
 BOARD_HAS_QCOM_WLAN              := true
